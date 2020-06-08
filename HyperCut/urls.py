a22341baa4c5699f2e160bbb91990dd45d3420e9
dkiryanov <dkiryanov@fix.ru>
2020-06-08 20:27:51 +0300

    changed admin path to not interfere with redirection

--- HyperCut/urls.py
+++ HyperCut/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('UrlCut.urls')),
-    path('admin/', admin.site.urls),
+    path('secret/admin/', admin.site.urls),
 ]
